6427a7f69425b983574ebfd3358dec19981542f7
satyakighosh <satyaki1997ghosh@gmail.com>
2022-05-08 12:18:41 +0530

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { ConfigureStore } from './AppState';
+import './FirebaseSetup';
 import GeekButton from './GeekButton';
-import RouteA from './RouteA';
-import SimpleState from './SimpleState';
+// import { Redirect } from 'react-router-dom';
+import Home from './Home';
 import ListNote from './ListNote';
-import UseEffectOnce from "./UseEffectOnce";
-import UseEffectMultiple from "./UseEffectMultiple";
+import Login from './Login';
+import MaterialUi from './MaterialUi';
 import PersonDisplay from "./PersonDisplay";
+import Profile from './Profile';
+import ReactHookFormUi from './ReactHookFormUi';
+import ReducerUi from './ReducerUi';
+import RouteA from './RouteA';
+import SignUp from './SignUp';
+import SimpleState from './SimpleState';
+import UseEffectMultiple from "./UseEffectMultiple";
+import UseEffectOnce from "./UseEffectOnce";
+import { Context } from './UserContext';
 import UseRefEx from "./UseRefEx";
 import WriteContext from './WriteContext';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './AppState';
 import WriteReducer from './WriteReducer';
-import ReducerUi from './ReducerUi';
-import ReactHookFormUi from './ReactHookFormUi';
-import MaterialUi from './MaterialUi';
-import SignUp from './SignUp';
-import Login from './Login';
-// import { Redirect } from 'react-router-dom';
-import Home from './Home'
-import './FirebaseSetup';
-import { Context } from './UserContext';
-import Profile from './Profile';
-import { useContext } from 'react';
 
 export default function App() {
   const context = useContext(Context);
@@ -30,49 +30,54 @@ export default function App() {
   return (
     <Provider store={ConfigureStore()}>
       <BrowserRouter>
-        
-          <Routes>
-            {/* routes tutorial */}
-            <Route path="/RouteA/:name" element={<RouteA />} />
-            <Route path="/SubmitButton"
-              element={() => <GeekButton name="submitName" label='submit' onClick={SubmitOnClick} />} />
-            <Route path="/RestButton"
-              element={() => <GeekButton name="resetName" label="reset" onClick={() => alert("reset button is invoked")} />} />
 
-            {/* state tutorial */}
-            <Route path="/SimpleState" element={<SimpleState />} />
-            <Route path="/ListNote" element={<ListNote />} />
+        <Routes>
+          {/* routes tutorial */}
+          <Route path="/RouteA/:name" element={<RouteA />} />
+          <Route path="/SubmitButton"
+            element={() => <GeekButton name="submitName" label='submit' onClick={SubmitOnClick} />} />
+          <Route path="/RestButton"
+            element={() => <GeekButton name="resetName" label="reset" onClick={() => alert("reset button is invoked")} />} />
 
-            {/* hooks tutorial */}
-            <Route path="/UseEffectOnce" element={<UseEffectOnce />} />
-            <Route path="/UseEffectMultiple" element={<UseEffectMultiple />} />
-            <Route path="/PersonDisplay" element={<PersonDisplay />} />
-            <Route path="/UseRefEx" element={<UseRefEx />} />
-            <Route path="/WriteContext" element={<WriteContext />} />
+          {/* state tutorial */}
+          <Route path="/SimpleState" element={<SimpleState />} />
+          <Route path="/ListNote" element={<ListNote />} />
 
-            {/* reducer tutorial */}
-            <Route path="/WriteReducer" element={<WriteReducer />} />
-            <Route path="/ReducerUi" element={<ReducerUi />} />
+          {/* hooks tutorial */}
+          <Route path="/UseEffectOnce" element={<UseEffectOnce />} />
+          <Route path="/UseEffectMultiple" element={<UseEffectMultiple />} />
+          <Route path="/PersonDisplay" element={<PersonDisplay />} />
+          <Route path="/UseRefEx" element={<UseRefEx />} />
+          <Route path="/WriteContext" element={<WriteContext />} />
 
-            {/* react hook form tutorial */}
-            <Route path="/ReactHookFormUi" element={<ReactHookFormUi />} />
+          {/* reducer tutorial */}
+          <Route path="/WriteReducer" element={<WriteReducer />} />
+          <Route path="/ReducerUi" element={<ReducerUi />} />
 
-            {/* material ui css tutorial */}
-            <Route path="/MaterialUi" element={<MaterialUi />} />
+          {/* react hook form tutorial */}
+          <Route path="/ReactHookFormUi" element={<ReactHookFormUi />} />
 
-            {/* project */}
-            {!isUserExist && <Route path="/SignUp" element={<SignUp />} />}
-            {!isUserExist && <Route path="/Login" element={<Login />} />}
-            <Route path="/Home" element={<Home />} />
+          {/* material ui css tutorial */}
+          <Route path="/MaterialUi" element={<MaterialUi />} />
 
-            {/* profile */}
-            {isUserExist && <Route path="/Profile" element={<Profile />} />}
-            
-            <Route path="*" element={<Home />} />
+          {/* project */}
+          {!isUserExist && <Route path="/SignUp" element={<SignUp />} />}
+          {!isUserExist && <Route path="/Login" element={<Login />} />}
+          <Route path="/Home" element={<Home />} />
+
+          {/* profile */}
+          {isUserExist && <Route path="/Profile" element={<Profile />} />}
+          <Route path="/" element={<Home />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
 
 
-          </Routes>
-      
+
+
+        </Routes>
+
       </BrowserRouter>
     </Provider>
   );
